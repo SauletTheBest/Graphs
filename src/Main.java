2634bfc023948d51f77db8e714c67a0acafd0725
Saulet <231003@astanait.edu.kz>
2024-05-27 23:00:33 +0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,42 @@
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyGraph<String> graph = new MyGraph<>();
+        graph.addVertex("Tokyo");
+        graph.addVertex("Kyoto");
+        graph.addVertex("Osaka");
+        graph.addVertex("Kobe");
+        graph.addVertex("Nagoya");
+        graph.addVertex("Hiroshima");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        graph.addEdge("Tokyo", "Nagoya", 4);
+        graph.addEdge("Tokyo", "Osaka", 8);
+        graph.addEdge("Nagoya", "Kyoto", 2);
+        graph.addEdge("Nagoya", "Osaka", 5);
+        graph.addEdge("Kyoto", "Osaka", 1);
+        graph.addEdge("Kyoto", "Kobe", 3);
+        graph.addEdge("Osaka", "Kobe", 2);
+        graph.addEdge("Osaka", "Hiroshima", 6);
+        graph.addEdge("Kobe", "Hiroshima", 4);
+
+        Vertex<String> source = graph.getVertex("Tokyo");
+        Vertex<String> targetKyoto = graph.getVertex("Kyoto");
+        Vertex<String> targetOsaka = graph.getVertex("Osaka");
+        Vertex<String> targetKobe = graph.getVertex("Kobe");
+        Vertex<String> targetHiroshima = graph.getVertex("Hiroshima");
+
+        System.out.println("BFS:");
+        BreadthFirstSearch<String> bfs = new BreadthFirstSearch<>(source);
+        System.out.println("Path from Tokyo to Kyoto: " + bfs.pathTo(targetKyoto));
+        System.out.println("Path from Tokyo to Osaka: " + bfs.pathTo(targetOsaka));
+        System.out.println("Path from Tokyo to Hiroshima: " + bfs.pathTo(targetHiroshima));
+
+        System.out.println("\nVertices in the graph:");
+        System.out.println(graph.getVertices());
+
+        System.out.println("\nDijkstra:");
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(source);
+        System.out.println("Path from Tokyo to Kyoto: " + dijkstra.pathTo(targetKyoto));
+        System.out.println("Path from Tokyo to Osaka: " + dijkstra.pathTo(targetOsaka));
+        System.out.println("Path from Tokyo to Hiroshima: " + dijkstra.pathTo(targetHiroshima));
     }
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -1,12 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Vertex<V> {
     private V data;
-    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+    private List<Edge<Vertex<V>>> adjacentEdges;
 
     public Vertex(V data) {
         this.data = data;
+        this.adjacentEdges = new ArrayList<>();
     }
 
     public V getData() {
@@ -14,16 +16,29 @@ public class Vertex<V> {
     }
 
     public void addAdjacentVertex(Vertex<V> destination, double weight) {
-        adjacentVertices.put(destination, weight);
+        Edge<Vertex<V>> edge = new Edge<>(this, destination, weight);
+        adjacentEdges.add(edge);
     }
 
-    public Map<Vertex<V>, Double> getAdjacentVertices() {
-        return adjacentVertices;
+    public List<Edge<Vertex<V>>> getAdjacentEdges() {
+        return adjacentEdges;
     }
 
     @Override
     public String toString() {
         return data.toString();
     }
-}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(data, vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
+}
